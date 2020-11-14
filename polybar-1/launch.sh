c61dfe457d4e67d3ae8819cafe8b0d58eb241549
cherrot <cherrotluo@gmail.com>
2020-11-15 04:56:15 +0800

    Finish tweak polybar

--- polybar-1/launch.sh
+++ polybar-1/launch.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env sh
-
-## Add this to your wm startup file.
+#!/bin/sh
 
 # Terminate already running bar instances
 killall -q polybar
@@ -9,5 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top -c ~/.config/polybar/config-top.ini &
-polybar bottom -c ~/.config/polybar/config-bottom.ini &
+polybar top &
+polybar bottom;
+
+echo "Bars launched..."
