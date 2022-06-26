f111bfe2af3efe449378fa704bcda26880dd70dd
cherrot <cherrotluo@gmail.com>
2022-06-26 12:38:30 +0800

    terminal: remove ugly macos configs. use 'include xxx' in future

--- install.sh
+++ install.sh
@@ -12,11 +12,7 @@ stow dotconfig
 if [[ "${OS}" == "Linux" ]]; then
     stow Xorg
     stow -t /etc _etc
-    stow alacritty.linux
-    stow kitty.linux
 elif [[ "${OS}" == "Darwin" ]]; then
     # stow dotconfig
     stow git
-    stow alacritty.macos
-    stow kitty.macos
 fi
