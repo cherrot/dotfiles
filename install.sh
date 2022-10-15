6c323170c0453c0a1ccc25ba5716d4de5cec243e
cherrot <cherrotluo@gmail.com>
2022-10-16 01:03:48 +0800

    [X11]: Use xft.dpi=144 to avoid xrandr 1.25 scaling.

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ elif [[ "${OS}" == "Linux" ]]; then
     if [[ $XDG_SESSION_TYPE == "x11" ]]; then
         stow home4X
         sudo stow -t /etc etc4X
+        sed -i 's/^font_size .*$/font_size 12.0/' home/.config/kitty/kitty.conf
     elif [[ $XDG_SESSION_TYPE == "wayland" ]]; then
         stow home4sway
         sudo stow -t /etc etc4sway
